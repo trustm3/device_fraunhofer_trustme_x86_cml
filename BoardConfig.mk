e00ad75ac5e248af6eb0b837f8b3970b6d68b71d
Michael Weiß <michael.weiss@aisec.fraunhofer.de>
2018-02-02 15:56:56 +0100

    Fixed start of adbd when selinux is disabled

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,4 +57,3 @@ TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
 BOARD_SEPOLICY_DIRS += build/target/board/generic/sepolicy
 BOARD_SEPOLICY_DIRS += build/target/board/generic_x86/sepolicy
 
-TARGET_USES_LOGD := false
